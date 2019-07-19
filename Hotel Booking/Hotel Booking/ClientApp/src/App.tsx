0a63231aca329496139442096d509475092fa52d
musta <loofey14@hotmail.com>
2019-07-19 22:52:40 +0200

    added react router router

--- Hotel Booking/Hotel Booking/ClientApp/src/App.tsx
+++ Hotel Booking/Hotel Booking/ClientApp/src/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      Hello!
+    <div className='App'>
+      <Router>
+        <Route exact path={'/'} component={Home}/>
+        <Route path={'/about'} component={About}/>
+      </Router>
+
     </div>
   );
 };
