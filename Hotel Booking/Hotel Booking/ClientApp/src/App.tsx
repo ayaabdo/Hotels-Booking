c0e156b7b7bc09f0c8aba0e3ab77fbe63e94f9b8
musta <loofey14@hotmail.com>
2019-07-21 14:34:54 +0200

    Add react router router

--- Hotel Booking/Hotel Booking/ClientApp/src/App.tsx
+++ Hotel Booking/Hotel Booking/ClientApp/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
+import 'semantic-ui-css/semantic.min.css'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
       <Router>
+        <Navbar/>
         <Route exact path={'/'} component={Home}/>
         <Route path={'/about'} component={About}/>
       </Router>
-
     </div>
   );
 };
